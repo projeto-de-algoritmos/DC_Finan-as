78f817f764a71d551e085d438ba6bccf76bae7ec
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-04-09 11:01:16 -0300

    Add vehicles data.

---
+++ museum-sort/src/data/vehicles.js
@@ -0,0 +1,26 @@
+// caminhão 3/4 : 29 m3
+// caminhão toco : 44 m3
+// caminhão truck : 58 m3
+// caminhão carreta : 97 m3
+export const vehicles = [
+  {
+    id: 1,
+    name: '3/4',
+    space: 29,
+  },
+  {
+    id: 2,
+    name: 'Toco',
+    space: 44,
+  },
+  {
+    id: 3,
+    name: 'Truck',
+    space: 58,
+  },
+  {
+    id: 4,
+    name: 'Carreta',
+    space: 97,
+  },
+];
